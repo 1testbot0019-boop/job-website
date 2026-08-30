222bd351774d8ed9be93c6fe9cb1c19e7fa5e108
1testbot0019-boop <1test.bot0019@gmail.com>
2026-08-30 19:59:22 +0530

    Add scheme crawler status metadata

---
+++ scraper/government_scheme_status.py
@@ -0,0 +1,3 @@
+"""Constants used to label scheme records as officially sourced."""
+SOURCE_POLICY = "official-government-only"
+SOURCE_SCOPE = "Central Government + 28 States + 8 Union Territories"
